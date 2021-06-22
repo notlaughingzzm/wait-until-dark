e9e8afcbc18db6ed7e32653841d63796f1422574
Zzm <34850800+notlaughingzzm@users.noreply.github.com>
2021-06-22 10:10:13 +0800

    <feat>修改base地址

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,68 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
+
+# # 确保脚本抛出遇到的错误
+# set -e
+
+# # 生成静态文件
+# npm run build
+
+# # 进入生成的文件夹
+# cd docs/.vuepress/dist
+
+# # deploy to github pages
+# echo 'b.chenzeliang.com' > CNAME
+
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy'
+#   githubUrl=git@github.com:chenzeliang/vuepress-theme-vdoing.git
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://chenzeliang:${GITHUB_TOKEN}@github.com/chenzeliang/vuepress-theme-vdoing.git
+#   git config --global user.name "chenzeliang"
+#   git config --global user.email "29351232@qq.com"
+# fi
+# git init
+# git add -A
+# git commit -m "${msg}"
+# git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+
+# # deploy to coding pages
+# # echo 'www.chenzeliang.com\nchenzeliang.com' > CNAME  # 自定义域名
+# # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+# # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+# #   codingUrl=git@e.coding.net:xgy/xgy.git
+# # else
+# #   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# # fi
+# # git add -A
+# # git commit -m "${msg}"
+# # git push -f $codingUrl master # 推送到coding
+
+# cd -
+# rm -rf docs/.vuepress/dist
+
 
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run build
+yarn build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+# 进入生成的文件夹，这里是默认的路径，可以自定义
+cd .vuepress/dist
 
-# deploy to github pages
-echo 'b.chenzeliang.com' > CNAME
+# 如果是发布到自定义域名
+# echo 'www.isunbeam.cn' > CNAME
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:chenzeliang/vuepress-theme-vdoing.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://chenzeliang:${GITHUB_TOKEN}@github.com/chenzeliang/vuepress-theme-vdoing.git
-  git config --global user.name "chenzeliang"
-  git config --global user.email "29351232@qq.com"
-fi
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+git commit -m 'deploy'
 
-# deploy to coding pages
-# echo 'www.chenzeliang.com\nchenzeliang.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
+git push -f git@github.com:notlaughingzzm/ master:blogs
 
 cd -
-rm -rf docs/.vuepress/dist
+
+# 最后发布的时候执行 bash deploy.sh
