00ca33e6c6fcbd74b1e47bfa11377b44bef516f7
Zzm <34850800+notlaughingzzm@users.noreply.github.com>
2021-06-22 10:17:33 +0800

    <feat>test

--- deploy.sh
+++ deploy.sh
@@ -61,7 +61,10 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
-git push -f git@github.com:notlaughingzzm/ master:blogs
+# git push -f git@github.com:notlaughingzzm/ master:blogs
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:notlaughingzzm/notlaughingzzm.github.io.git master
 
 cd -
 
